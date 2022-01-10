470ca48270a72ef209891d1f65907266820e2364
German <german.herrera@gmail.com>
2022-01-10 11:20:17 -0300

    clean

--- clean molinos.sql
+++ clean molinos.sql
@@ -4,3 +4,5 @@ DELETE FROM etapa_tareas;
 DELETE FROM molino_etapas;
 DELETE FROM turno_historial;
 UPDATE molino_partes SET botadas=0, limpiadas=0, montadas=0, total_botadas=0, total_limpiadas=0, total_montadas=0;
+UPDATE molinos SET estado=NULL, etapa=NULL;
+UPDATE turnos SET estado=NULL, turno_id=NULL;
