b544c72c0c6385b9387c80dc10a1c5a210fd4e1d
German <german.herrera@gesintel.cl>
2022-04-19 19:05:34 -0400

    spring 2.6.6

--- back/src/main/java/com/htg/mills/MillsApiApplication.java
+++ back/src/main/java/com/htg/mills/MillsApiApplication.java
@@ -5,9 +5,11 @@ import java.util.concurrent.TimeUnit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.http.CacheControl;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@EnableWebMvc
 @SpringBootApplication
 public class MillsApiApplication implements WebMvcConfigurer {
 
@@ -18,7 +20,6 @@ public class MillsApiApplication implements WebMvcConfigurer {
 	@Override
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
 
-	    // Register resource handler for images
 	    registry.addResourceHandler("/**").addResourceLocations("classpath:/static/")
 	            .setCacheControl(CacheControl.maxAge(2, TimeUnit.HOURS).cachePublic());
 	}
